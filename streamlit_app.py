04ec69e3abea1f172e970e799d7f29d9ddacf253
arturcefalo-star <arturcefalo@gmail.com>
2026-07-15 18:17:01 +0000

    676767

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,97 +1,144 @@
 import streamlit as st
+import time
+import random
+from streamlit_autorefresh import st_autorefresh
 
-st.title("Clicker Game (Beta)")
+if "mostrar_foto" not in st.session_state:
+    st.session_state.mostrar_foto = False
+if "pontos" not in st.session_state:
+    st.session_state.pontos = 0
+if "foto_sorteada" not in st.session_state:
+    st.session_state.foto_sorteada = None
+if "sorteado" not in st.session_state:
+    st.session_state.sorteado = True
+
+col3, col4, col5, col6 = st.columns(4)
 
 if "pontos" not in st.session_state:
     st.session_state.pontos = 0
-
 if "poder_clique" not in st.session_state:
     st.session_state.poder_clique = 1
+if "pontos_por_segundo" not in st.session_state:
+    st.session_state.pontos_por_segundo = 0
+if "ultimo_tick" not in st.session_state:
+    st.session_state.ultimo_tick = time.time()
 
+def comprar_melhoria(custo, tipo, qtd):
+    if st.session_state.pontos >= custo:
+        st.session_state.pontos = max(0, st.session_state.pontos - custo)
+        if tipo == "clique":
+            st.session_state.poder_clique += qtd
+        elif tipo == "passivo":
+            st.session_state.pontos_por_segundo += qtd 
+        time.sleep(0.5)
+        st.rerun()
+
+if st.session_state.pontos_por_segundo > 0:
+    st_autorefresh(interval=900, key="autoclicker")
+
+agora = time.time()
+tempo_passado = agora - st.session_state.ultimo_tick
+
+if tempo_passado >= 1.0:
+    ciclos = int(tempo_passado)
+    st.session_state.pontos += st.session_state.pontos_por_segundo * ciclos
+    st.session_state.ultimo_tick = agora - (tempo_passado - ciclos)
 def adicionar_ponto():
     st.session_state.pontos += st.session_state.poder_clique
 
-def comprar_melhoria():
-    if st.session_state.pontos >= 100:
-        st.session_state.pontos -= 100
-        st.session_state.poder_clique += 1
-    else:
-        st.session_state.erro_dinheiro = True
+#COLUNA 
+melhorias_clique = [
+    {"qtd": 1, "custo": 1},
+    {"qtd": 5, "custo": 500},
+    {"qtd": 10, "custo": 1000},
+    {"qtd": 50, "custo": 5000},
+    {"qtd": 100, "custo": 10000},
+    {"qtd": 500, "custo": 50000},
+    {"qtd": 1000, "custo": 100000},
+    {"qtd": 2500, "custo": 250000},
+    {"qtd": 5000, "custo": 500000},
+    {"qtd": 10000, "custo": 1000000},
+]
 
-st.image("logo.png", width=200)
+#COLUNA 2
+melhorias_passivas = [
+    {"qtd": 1, "custo": 2},
+    {"qtd": 5, "custo": 1000},
+    {"qtd": 10, "custo": 4000},
+    {"qtd": 50, "custo": 20000},
+    {"qtd": 100, "custo": 100000},
+    {"qtd": 500, "custo": 200000},
+]
 
-st.button("            Click Here          ", on_click=adicionar_ponto)
+st.title("Clicker Game (Beta)")
+st.write("Trilha sonora: on/off ")
+st.audio("musica67.mp3")
+
+if st.button("            Click Here          "):
+    st.session_state.pontos += st.session_state.poder_clique
 
 st.write(f"Pontos: {st.session_state.pontos}")
 st.write(f"Poder de clique: {st.session_state.poder_clique}")
+st.write(f"Pontos por segundo: {st.session_state.pontos_por_segundo}")
 
-st.button("Melhoria +1 = 100 Pontos", on_click=comprar_melhoria, use_container_width=True)
+with col3:
+    if st.button("Abrir Ovo = 1 Pontos"):
+        if st.session_state.pontos >= 1:
+            st.session_state.pontos -= 1
 
-if "erro_dinheiro" in st.session_state and st.session_state.erro_dinheiro:
-    st.error("Pontos Insuficiente")
-    st.session_state.erro_dinheiro = False
+            sorteado = random.choices(
+               [{"nome": "Siruriru", "arquivo": "logo3.png", "bonus": 1, "chance": "50%"}, 
+                {"nome": "Peppa Pig", "arquivo": "logo2.png", "bonus": 5, "chance": "35%"},
+                {"nome": "Manoel G", "arquivo": "logo1.png", "bonus": 10, "chance": "15%"}],
+               weights = [50, 35, 15], k=1
+            )[0]
+            st.session_state.foto_sorteada = sorteado
 
-def comprar_melhoria():
-    if st.session_state.pontos >= 500:
-        st.session_state.pontos -= 500
-        st.session_state.poder_clique += 5
-    else:
-        st.session_state.erro_dinheiro = True
+            st.session_state.poder_clique += sorteado["bonus"]
+        else:
+            st.warning("Pontos insuficiente")
 
-st.button("Melhoria +5 = 500 Pontos", on_click=comprar_melhoria, use_container_width=True)
+    if "foto_sorteada" in st.session_state and st.session_state.foto_sorteada:
+        ganhou = st.session_state.foto_sorteada
 
-if "erro_dinheiro" in st.session_state and st.session_state.erro_dinheiro:
-    st.error("Pontos Insuficiente")
-    st.session_state.erro_dinheiro = False
+        if "arquivo" in ganhou:
+            st.write(f"Seus Pets:")
+            st.image(ganhou["arquivo"], width=100)
+            st.write(f"{ganhou.get('chance')} - {ganhou['nome']} + {ganhou.get('bonus')} Ponto(s)")
 
-def comprar_melhoria():
-    if st.session_state.pontos >= 1000:
-        st.session_state.pontos -= 1000
-        st.session_state.poder_clique += 10
-    else:
-        st.session_state.erro_dinheiro = True
+st.markdown("---")
 
-st.button("Melhoria +10 = 1000 Pontos", on_click=comprar_melhoria, use_container_width=True)
+col1, col2 = st.columns(2)
 
-if "erro_dinheiro" in st.session_state and st.session_state.erro_dinheiro:
-    st.error("Pontos Insuficiente")
-    st.session_state.erro_dinheiro = False
+#COLUNA 1:
+with col1:
+    st.subheader("Melhoria Clicker")
+    for item in melhorias_clique:
+        texto = f"Melhoria +{item['qtd']} = {item['custo']} Pontos"
 
-def comprar_melhoria():
-    if st.session_state.pontos >= 5000:
-        st.session_state.pontos -= 5000
-        st.session_state.poder_clique += 50
-    else:
-        st.session_state.erro_dinheiro = True
+        desativado = st.session_state.pontos < item['custo']
 
-st.button("Melhoria +50 = 5000 Pontos", on_click=comprar_melhoria, use_container_width=True)
+        if st.button(texto, key=f"clique_{item['qtd']}", disabled=desativado):
+            time.sleep(0.1) 
+            if st.session_state.pontos >= item['custo']:
+                st.session_state.pontos -= item['custo']
+                st.session_state.poder_clique += item['qtd']
+                st.rerun()
+            else:
+                st.warning("Pontos insuficiente")
 
-if "erro_dinheiro" in st.session_state and st.session_state.erro_dinheiro:
-    st.error("Pontos Insuficiente")
-    st.session_state.erro_dinheiro = False
+with col2:
+    st.subheader("Auto Clickers")
+    for item in melhorias_passivas:
+        texto = f"Gerador +{item['qtd']}/s = {item['custo']} Pontos"
 
-def comprar_melhoria():
-    if st.session_state.pontos >= 10000:
-        st.session_state.pontos -= 10000
-        st.session_state.poder_clique += 100
-    else:
-        st.session_state.erro_dinheiro = True
+        desativado = st.session_state.pontos < item['custo']
 
-st.button("Melhoria +100 = 10000 Pontos", on_click=comprar_melhoria, use_container_width=True)
-
-if "erro_dinheiro" in st.session_state and st.session_state.erro_dinheiro:
-    st.error("Pontos Insuficiente")
-    st.session_state.erro_dinheiro = False
-
-def comprar_melhoria():
-    if st.session_state.pontos >= 25000:
-        st.session_state.pontos -= 25000
-        st.session_state.poder_clique += 250
-    else:
-        st.session_state.erro_dinheiro = True
-
-st.button("Melhoria +250 = 25000 Pontos", on_click=comprar_melhoria, use_container_width=True)
-
-if "erro_dinheiro" in st.session_state and st.session_state.erro_dinheiro:
-    st.error("Pontos Insuficiente")
-    st.session_state.erro_dinheiro = False
+        if st.button(texto, key=f"passivo_{item['qtd']}", disabled=desativado):
+            time.sleep(0.1)
+            if st.session_state.pontos >= item['custo']:
+                st.session_state.pontos -= item['custo']
+                st.session_state.pontos_por_segundo += item['qtd']
+                st.rerun()
+            else:
+                st.warning("Pontos insuficiente")
