00330115fbc8f4f5d8c6bedc6c8c2410ef024696
arturcefalo-star <arturcefalo@gmail.com>
2026-07-19 19:48:51 -0300

    Change developer password format

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ BONUS_PET_M2_R3 = 50000000
 CUSTO_OVO_MUNDO_2_CARO = 500000000   # Custo do segundo ovo do Mundo 2
 # =====================================================================
 
-SENHA_DEV = "$3CR3T"  
+SENHA_DEV = "/--$3CR3T--\"  
 SENHA_ADMIN = "XXxx67xxXX"
 SENHA_APOIADOR = "67AP0IO67"  
 ACCOUNTS_FILE = "usuarios.json"
